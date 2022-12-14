10ce07dc5e6ec5c38d8229077a496657e5c1b957
Brent Allen <brenta@microsoft.com>
2022-12-13 16:55:18 -0800

    day11

--- challenges/2022/day11/src/main.rs
+++ challenges/2022/day11/src/main.rs
@@ -18,7 +18,7 @@ use serde_derive::Deserialize;
 enum Expression {
     Add(Box<Expression>, Box<Expression>),
     Multiply(Box<Expression>, Box<Expression>),
-    Constant(i32),
+    Constant(Worries),
     Variable(String),
 }
 
@@ -35,13 +35,13 @@ impl Expression {
                 Box::new(Expression::parse(&a)),
                 Box::new(Expression::parse(&b)),
             )
-        } else if let Ok(a) = scan_fmt!(trimmed, "{d}", i32) {
+        } else if let Ok(a) = scan_fmt!(trimmed, "{d}", Worries) {
             Expression::Constant(a)
         } else {
             Expression::Variable(trimmed.to_string())
         }
     }
-    pub fn evaluate(&self, variables: &HashMap<String, i32>) -> i32 {
+    pub fn evaluate(&self, variables: &HashMap<String, Worries>) -> Worries {
         match self {
             Expression::Add(a, b) => a.evaluate(variables) + b.evaluate(variables),
             Expression::Multiply(a, b) => a.evaluate(variables) * b.evaluate(variables),
@@ -53,33 +53,41 @@ impl Expression {
 
 #[derive(Debug)]
 enum Test {
-    DivisibleBy(i32),
+    DivisibleBy(Worries),
 }
+
+type Worries = u64;
 
 impl Test {
     pub fn parse(str: &str) -> Test {
-        if let Ok(v) = scan_fmt!(str, "divisible by {}", i32) {
+        if let Ok(v) = scan_fmt!(str, "divisible by {}", Worries) {
             Test::DivisibleBy(v)
         } else {
             panic!("Unexpected test value");
         }
     }
 
-    pub fn evaluate(&self, value: i32) -> bool {
+    pub fn evaluate(&self, value: Worries) -> bool {
         match self {
             Test::DivisibleBy(v) => value % *v == 0,
         }
     }
+
+    pub fn divisible_amount(&self) -> Option<Worries> {
+        match self {
+            Test::DivisibleBy(v) => Some(*v),
+        }
+    } 
 }
 
 #[derive(Debug)]
 enum Action {
-    ThrowTo(i32),
+    ThrowTo(usize),
 }
 
 impl Action {
     pub fn parse(str: &str) -> Action {
-        if let Ok(v) = scan_fmt!(str, "throw to monkey {}", i32) {
+        if let Ok(v) = scan_fmt!(str, "throw to monkey {}", usize) {
             Action::ThrowTo(v)
         } else {
             panic!("Unexpected action value: {}", str);
@@ -89,12 +97,12 @@ impl Action {
 
 #[derive(Debug)]
 struct Monkey {
-    items: VecDeque<i32>,
+    items: VecDeque<Worries>,
     operation: Expression,
     test: Test,
     true_action: Action,
     false_action: Action,
-    inspection_count: i32,
+    inspection_count: usize,
 }
 
 
@@ -104,8 +112,8 @@ fn parse_input(input: &str) -> Vec<RefCell<Monkey>> {
     let mut line_iterator = input.lines();
 
     while let Some(monkey_line) = line_iterator.next() {
-        let monkey_index = scan_fmt!(monkey_line, "Monkey {}:", i32).unwrap();
-        assert!(monkey_index as usize == monkeys.len());
+        let monkey_index = scan_fmt!(monkey_line, "Monkey {}:", usize).unwrap();
+        assert!(monkey_index == monkeys.len());
         let items_str = scan_fmt!(
             line_iterator.next().unwrap(),
             "  Starting items: {[0-9, ]}",
@@ -115,7 +123,7 @@ fn parse_input(input: &str) -> Vec<RefCell<Monkey>> {
         
         let items = items_str
             .split(",")
-            .map(|i| i.trim().parse::<i32>().unwrap())
+            .map(|i| i.trim().parse::<Worries>().unwrap())
             .collect();
         let operation_str = scan_fmt!(
             line_iterator.next().unwrap(),
@@ -148,9 +156,9 @@ fn parse_input(input: &str) -> Vec<RefCell<Monkey>> {
 }
 
 fn part1(input: &str) -> String {
-    let mut input = parse_input(input);
+    let input = parse_input(input);
 
-    println!("Monkeys: {input:?}");
+    //println!("Monkeys: {input:?}");
 
     for round in 0..20 {
         for (index, monkey) in input.iter().enumerate() {
@@ -179,36 +187,84 @@ fn part1(input: &str) -> String {
                         target_monkey.items.push_back(post_inspection);
 
                         if round == 0 {
-                            println!("Monkey {index}: {item} became {new} and then {post_inspection}, thrown to {target}")
+                            //println!("Monkey {index}: {item} became {new} and then {post_inspection}, thrown to {target}")
+                        }
+                    }
+                }
+            }
+        }
+/* 
+        println!("Round {}", round + 1);
+        for (i, monkey) in input.iter().enumerate() {
+            println!("Monkey {}: {:?}", i, monkey.borrow().items);
+        }
+        */
+    }
+    let mut top_count :Vec<_> = input.iter().map(|m| m.borrow().inspection_count).collect();
+    top_count.sort();
+    println!("top_count {top_count:?}");
+    let monkey_business : usize = top_count.iter().rev().take(2).product();
+    monkey_business.to_string()
+}
+
+fn part2(input: &str) -> String {
+    let  input = parse_input(input);
+    //println!("Monkeys: {input:?}");
+
+    let cap : Worries = input.iter().map(|m| m.borrow().test.divisible_amount().unwrap()).product();
+
+    for round in 0..10000 {
+        for (index, monkey) in input.iter().enumerate() {
+            let mut current_monkey = monkey.borrow_mut();
+
+            while let Some(item) = current_monkey.items.pop_front() {
+                let mut variables = HashMap::new();
+                variables.insert("old".to_string(), item);
+
+                // Inspect
+                let new = current_monkey.operation.evaluate(&variables);
+                //let post_inspection = new / 3;
+                let post_inspection = new % cap;
+                current_monkey.inspection_count += 1;
+
+                // Test
+                let action = if current_monkey.test.evaluate(post_inspection) {
+                    &current_monkey.true_action
+                } else {
+                    &current_monkey.false_action
+                };
+
+                // Act
+                match action {
+                    Action::ThrowTo(target) => {
+                        let mut target_monkey = input[*target as usize].borrow_mut();
+                        target_monkey.items.push_back(post_inspection);
+
+                        if round == 0 {
+                            //println!("Monkey {index}: {item} became {new} and then {post_inspection}, thrown to {target}")
                         }
                     }
                 }
             }
         }
 
-        println!("Round {}", round + 1);
+        /*println!("Round {}", round + 1);
         for (i, monkey) in input.iter().enumerate() {
             println!("Monkey {}: {:?}", i, monkey.borrow().items);
-        }
+        }*/
     }
     let mut top_count :Vec<_> = input.iter().map(|m| m.borrow().inspection_count).collect();
     top_count.sort();
     println!("top_count {top_count:?}");
-    let monkey_business : i32 = top_count.iter().rev().take(2).product();
+    let monkey_business : usize = top_count.iter().rev().take(2).product();
     monkey_business.to_string()
-}
-
-fn part2(input: &str) -> String {
-    let input = parse_input(input);
-
-    "".to_string()
 }
 
 fn main() {
     let example_input = aoc::load_input(env!("CARGO_MANIFEST_DIR"), "sample.txt");
 
     let expected_example_part1 = "10605";
-    let expected_example_part2 = "?";
+    let expected_example_part2 = "2713310158";
 
     println!("AOC 2022 {}", env!("CARGO_PKG_NAME"));
     println!("Sample Part 1:");
---
+++ challenges/2022/day12/src/main.rs
@@ -0,0 +1,183 @@
+use std::{
+    cell::{Cell, RefCell},
+    collections::{HashMap, HashSet, VecDeque},
+    fmt::Display,
+    hash::Hash,
+    rc::{Rc, Weak}, borrow::Borrow, ops::Sub, slice::SliceIndex,
+};
+
+use aoc::{
+    coordinate::{Coordinate, RowCol},
+    grid::Grid,
+    position::{Direction, Position, EACH_DIRECTION},
+};
+use scan_fmt::scan_fmt;
+use serde_derive::Deserialize;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Default, Clone, Copy)]
+struct Height(char);
+
+impl Height {
+    pub fn new(c: char) -> Self {
+        Self(c)
+    }
+}
+
+impl Sub for Height {
+    type Output = i32;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.0 as i32 - rhs.0 as i32
+    }
+}
+
+impl Display for Height {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+
+fn parse_input(input: &str) -> (RowCol, RowCol, Grid<Height, RowCol>) {
+    let mut heights = Grid::new();
+
+    let mut row = 0;
+
+    let mut start = None;
+    let mut end = None;
+    for line in input.lines() {
+        let mut col = 0;
+        for c in line.chars() {
+            let coordinate = RowCol::new(row, col);
+            if c == 'S' {
+                start = Some(coordinate.clone());
+            }
+            if c == 'E' {
+                end = Some(coordinate.clone());
+            }
+
+            let h = if c == 'S' { Height::new('a') } else if c == 'E' { Height::new('z') } else { Height::new(c) };
+            *heights.get_mut_or_default(&coordinate) = h;
+
+            col += 1;
+        }
+        row += 1;
+    }
+
+    (start.unwrap(), end.unwrap(), heights)
+}
+
+
+fn fill_distances(heights: &Grid<Height, RowCol>, distances: &mut Grid<usize, RowCol>, position: RowCol, distance: usize)
+{
+    if let Some(existing_distance) = distances.get(&position) {
+        if *existing_distance <= distance {
+            return;
+        }
+    }
+
+    *distances.get_mut_or_default(&position) = distance;
+
+    let current_height = heights.get(&position).unwrap();
+
+    for d in EACH_DIRECTION {
+        let next = position.project(d, 1);
+        if heights.range().contains(&next) {
+            let next_height = heights.get(&next).unwrap();
+
+            let step_up = *current_height - *next_height;
+            if step_up <= 1 {
+                fill_distances(heights, distances, next, distance + 1);
+            }
+        }
+    }
+}
+fn part1(input: &str) -> String {
+    let (start, end, heights) = parse_input(input);
+
+    let mut distances = Grid::new();
+
+    fill_distances(&heights, &mut distances, end, 0);
+
+    distances.get(&start).unwrap().to_string()
+}
+
+fn part2(input: &str) -> String {
+    let (start, end, heights) = parse_input(input);
+
+    let mut distances = Grid::new();
+
+    fill_distances(&heights, &mut distances, end, 0);
+
+    let mut best = None;
+    for (c, t) in heights.enumerate_tiles() {
+        if *t.unwrap() == Height::new('a') {
+            if let Some(d) = distances.get(&c) {
+                if let Some(best_distance) = best {
+                    if d < best_distance {
+                        best = Some(d)
+                    }
+                } else {
+                    best = Some(d);
+                }
+            }
+        }
+    }
+
+    best.unwrap().to_string()
+}
+
+fn main() {
+    let example_input = aoc::load_input(env!("CARGO_MANIFEST_DIR"), "sample.txt");
+
+    let expected_example_part1 = "31";
+    let expected_example_part2 = "29";
+
+    println!("AOC 2022 {}", env!("CARGO_PKG_NAME"));
+    println!("Sample Part 1:");
+    let sample_result_part1 = part1(&example_input);
+    if sample_result_part1 != expected_example_part1 {
+        println!(
+            "  Answer: {} (expected {})",
+            sample_result_part1, expected_example_part1
+        );
+    } else {
+        println!("  Answer: {} CORRECT!!!", sample_result_part1);
+    }
+    println!("Sample Part 2:");
+    let sample_result_part2 = part2(&example_input);
+    if sample_result_part2 != expected_example_part2 {
+        println!(
+            "  Answer: {} (expected {})",
+            sample_result_part2, expected_example_part2
+        );
+    } else {
+        println!("  Answer: {} CORRECT!!!", sample_result_part2);
+    }
+    println!("");
+
+    let run_input = true;
+    if run_input {
+        let input = aoc::load_input(env!("CARGO_MANIFEST_DIR"), "input.txt");
+
+        println!("Puzzle Part 1:");
+        let result = part1(&input);
+        println!("  Answer: {}", result);
+        println!("Puzzle Part 2:");
+        let result = part2(&input);
+        println!("  Answer: {}", result);
+        println!("");
+
+        if sample_result_part1 == expected_example_part1 {
+            println!("EXAMPLE PART 1 CORRECT");
+        } else {
+            println!("EXAMPLE PART 1 FAILED");
+        }
+
+        if sample_result_part2 == expected_example_part2 {
+            println!("EXAMPLE PART 2 CORRECT");
+        } else {
+            println!("EXAMPLE PART 2 FAILED");
+        }
+    }
+}
